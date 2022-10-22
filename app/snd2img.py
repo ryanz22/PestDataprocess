6af845b02f1c80115059b0004106dc9d74a60eba
ryanz22 <ryanzhang106@gmail.com>
2022-10-21 19:52:39 -0700

    remove argparse code

--- app/snd2img.py
+++ app/snd2img.py
@@ -1,6 +1,5 @@
 import sys
 import os
-import argparse
 from timeit import default_timer as timer
 from datetime import timedelta
 import psutil
@@ -91,86 +90,12 @@ def test():
     pass
 
 
-def main(args):
-    match args.command:
-        case 'extract':
-            out_dir = args.out_dir
-            if not os.path.exists(out_dir):
-                print(f'create folder {out_dir}')
-                os.mkdir(out_dir)
-            else:
-                print(f'folder {out_dir} exists')
-
-            flist = ['/btbwar/XC139608.mp3', '/btbwar/XC51863.mp3', '/btbwar/XC134502.mp3', '/btbwar/XC415596.mp3']
-            plist = [ f'{TRAIN_DIR}{f}' for f in flist ]
-            print(f'plist: {plist}')
-            start = timer()
-            core_cnt = psutil.cpu_count(logical=False)
-            print(f'this computer has {core_cnt} physical cores')
-            # 27s to finish
-            batch_extract(plist, out_dir, batch=3, thres=args.threshold, imgsize=args.imgsize)
-
-            # 41s to finish
-            # for f in plist:
-            #     scaleo_extract(f, outdir=out_dir)
-
-            # 30s to finish
-            # ray.init(num_cpus=4)
-            # # fn_id = ray.put()
-            # ray.get([scaleo_extract.remote(ray.put(f), outdir=out_dir) for f in plist])
-            end = timer()
-            print(f'======== total time: {timedelta(seconds=end-start)}')
-        case 'single_extract':
-            f = args.file
-            out_dir = args.out_dir
-            if not os.path.exists(out_dir):
-                print(f'create folder {out_dir}')
-                os.mkdir(out_dir)
-            else:
-                print(f'folder {out_dir} exists')
-            scaleo_extract(f, outdir=out_dir, thres=args.threshold, img_size=args.imgsize)
-        case 'test':
-            pass
-        case _:
-            print(f'unknown command [{args.command}]')
-
 if __name__ == '__main__':
     print(f'python version is {sys.version_info}')
     if not (sys.version_info.major == 3 and sys.version_info.minor >= 10):
         sys.exit('this program needs python 3.10 and above to run')
 
     # https://towardsdatascience.com/a-simple-guide-to-command-line-arguments-with-argparse-6824c30ab1c3
-    # aparser = argparse.ArgumentParser(description='CWT Utility')
-    # sub = aparser.add_subparsers(dest='command', help='command to run', required=True)
-    # extract_arg = sub.add_parser('extract', help='extract signature of a batch')
-    # extract_arg.add_argument('-i', '--input_dir', type=str, help='input folder',
-    #                       required=True)
-    # extract_arg.add_argument('-o', '--out_dir', type=str, help='output folder',
-    #                       required=True)
-    # extract_arg.add_argument('-t', '--threshold', type=int, default=-30,
-    #                     help='threshold in dB, default -30', required=True)
-    # extract_arg.add_argument('-s', '--imgsize', type=int, default=512,
-    #                     help='image size, default 512', required=True)
-    # single_arg = sub.add_parser('single_extract', help='extract signature of a single file')
-    # single_arg.add_argument('-f', '--file', type=str, help='file path',
-    #                       required=True)
-    # single_arg.add_argument('-o', '--out_dir', type=str, help='output folder',
-    #                       required=True)
-    # single_arg.add_argument('-t', '--threshold', type=int, default=-30,
-    #                     help='threshold in dB, default -30', required=True)
-    # single_arg.add_argument('-s', '--imgsize', type=int, default=512,
-    #                     help='image size, default 512', required=True)
-    # test_arg = sub.add_parser('test', help='test functions')
-    # test_arg.add_argument('-d', '--dir', type=str, help='file directory',
-    #                       required=True)
-    # # aparser.add_argument('-s', '--start', type=int, help='start page',
-    # #                      required=True)
-    # # aparser.add_argument('-o', '--output', type=str, help='output file name',
-    # #                      required=True)
-    # targs = aparser.parse_args()
-
-    # main(targs)
-
     # print(f'sys.path:\n{sys.path}')
 
     l_fmt = '[%(name)s %(levelname)s] %(asctime)s - %(message)s'
--- app/snd_tool.py
+++ app/snd_tool.py
@@ -1,6 +1,5 @@
 import os
 import sys
-import argparse
 import pathlib
 import numpy as np
 import pprint
@@ -8,7 +7,6 @@ from typing import Tuple
 import librosa
 import click
 import soundfile as sf
-from scipy.io import wavfile
 import logging
 
 from dataprocess.util.file import append_suffix, change_ext
@@ -17,46 +15,6 @@ from dataprocess.sound.preprocess import (denoise as deno, to_mono, sound_file_i
 from dataprocess.sound.filter_util import (load_audio_file, freq_filter)
 import warnings
 warnings.filterwarnings('ignore') # get rid of librosa warnings
-
-
-def main(args):
-    match args.command:
-        case 'denoise':
-            in_fn = args.input_file
-            # sr, data = wavfile.read(in_fn)
-            data, sr = librosa.load(in_fn, sr=None, mono=False)
-            od, sr = denoise(data, sr)
-            out_fn = append_suffix(in_fn, 'denoised')
-            print(f'output file name: {out_fn}')
-            # wavfile.write(out_fn, sr, od)
-            sf.write(out_fn, od, sr)
-        case 'mono':
-            in_fn = args.input_file
-            if is_stereo_sound(in_fn):
-                data, sr = librosa.load(in_fn, sr=None, mono=False)
-                od, sr = to_mono(data, sr)
-                out_fn = append_suffix(in_fn, 'mono')
-                if pathlib.Path(in_fn).suffix != '.wav':
-                    out_fn = change_ext(out_fn, '.wav')
-                sf.write(out_fn, od, sr)
-            else:
-                print('this is a mono sound track')
-        case 'resample':
-            in_fn = args.input_file
-            tsr = args.sr
-            data, sr = librosa.load(in_fn, sr=None, mono=False)
-            od, nsr = resample(data, sr, tsr=tsr)
-            out_fn = append_suffix(in_fn, str(nsr))
-            if pathlib.Path(in_fn).suffix != '.wav':
-                out_fn = change_ext(out_fn, '.wav')
-            sf.write(out_fn, od, nsr)
-        case 'info':
-            t = sound_file_info(args.file_name)
-            pprint.pprint(t, indent=2)
-        case 'filter':
-            filter(args)
-        case _:
-            print(f'unknown command [{args.command}]')
 
 
 @click.group()
@@ -191,71 +149,6 @@ if __name__ == '__main__':
         sys.exit('this program needs python 3.10 and above to run')
 
     # https://towardsdatascience.com/a-simple-guide-to-command-line-arguments-with-argparse-6824c30ab1c3
-    # aparser = argparse.ArgumentParser(description='CWT Utility')
-    # sub = aparser.add_subparsers(dest='command', help='command to run', required=True)
-
-    # denoise_arg = sub.add_parser('denoise', help='denoise a sound file')
-    # denoise_arg.add_argument('-i', '--input_file', type=str, help='input file',
-    #                       required=True)
-
-    # mono_arg = sub.add_parser('mono', help='convert stereo to mono')
-    # mono_arg.add_argument('-i', '--input_file', type=str, help='input file',
-    #                       required=True)
-
-    # resample_arg = sub.add_parser('resample', help='convert sample rate')
-    # resample_arg.add_argument('-i', '--input_file', type=str, help='input file',
-    #                       required=True)
-    # resample_arg.add_argument('-r', '--sr', type=int, help='output sample rate, default 22050',
-    #                       default=22050, required=True)
-
-    # info_arg = sub.add_parser('info', help='get sound file info')
-    # info_arg.add_argument('-f', '--file_name', type=str, help='file name',
-    #                       required=True)
-
-    # filter_arg = sub.add_parser('filter', help='filter sound')
-    # filter_arg.add_argument(
-    #     '-f', '--file',
-    #     type=str,
-    #     required=True,
-    #     help="Prefilter sound file location, default = None",
-    # )
- 
-    # filter_arg.add_argument(
-    #     '-t', '--type',
-    #     type=str,
-    #     required=True,
-    #     choices=['lowpass', 'highpass', 'bandpass', 'bandstop' ],
-    #     help="Type of filter, options: lowpass, highpass, bandpass, bandstop",
-    # )
-
-    # filter_arg.add_argument(
-    #     "--fc",
-    #     type=float,
-    #     help="Cutoff frequency",
-    # )
-
-    # filter_arg.add_argument(
-    #     "--fl",
-    #     type=float,
-    #     help="Lower cutoff frequency",
-    # )
-
-    # filter_arg.add_argument(
-    #     "--fh",
-    #     type=float,
-    #     help="Upper cutoff frequency",
-    # )
-    
-    # filter_arg.add_argument(
-    #     "--sample_rate",
-    #     type=float,
-    #     default=22050.0,
-    #     help="Sampling rate, default: 22050.0",
-    # )
-    # targs = aparser.parse_args()
-
-    # main(targs)
-
     print(f'sys.path:\n{sys.path}')
 
     # l_fmt = '[%(levelname)s] %(asctime)s - %(message)s'
